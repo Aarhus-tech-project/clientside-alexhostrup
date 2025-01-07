e1cf4a1d64d95ed104aa8d1e6526acd3dd0d79fe
Alexander Hostrup <alexander.hostrup@gmail.com>
2025-01-07 22:57:02 +0100

    Fixed some issues with the timestamp

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,25 @@ import { initPlayer } from './utils/player';
 import { initMountains } from './graphics/mountain';
 import { updateEntities } from './utils/updater';
 import { initObstacles } from './handlers/obstacleHandler';
-import { isRunning, setIsRunning } from './handlers/gameOver';
+import { gameOver, isRunning, setIsRunning } from './handlers/gameOver';
+import { initScore } from './graphics/ui';
+import { addToDebugInfo } from './utils/debug';
 
 export let canvas: HTMLCanvasElement;
 export let context: CanvasRenderingContext2D;
 export let delta: number
 export let fps: number
-let oldTimeStamp: number
+let currentTimeStampStart: number | null
+let oldTimeStamp: number | null
 
-const init = () => {
+export const init = () => {
   canvas = document.querySelector<HTMLCanvasElement>('#gameCanvas')!
   context = canvas.getContext('2d')!;
 
   canvas.height = window.innerHeight / 1.33
   canvas.width = window.innerWidth / 1.33
 
+  initScore()
   initializeInput()
   initPlayer()
   initMountains()
@@ -31,13 +35,25 @@ const init = () => {
 }
 
 const update = (timeStamp: number) => {
-  if (!isRunning) return
-  delta = (timeStamp - oldTimeStamp) / 1000
-  oldTimeStamp = timeStamp
+  if (!isRunning) {
+    currentTimeStampStart = null
+    oldTimeStamp = null
+    gameOver()
+    return
+  }
+
+  if (!currentTimeStampStart) currentTimeStampStart = timeStamp
+
+  addToDebugInfo(`TimeStamp (old): ${timeStamp}`)
+  const currentTimeStamp = timeStamp - currentTimeStampStart
+  if (oldTimeStamp) {
+    delta = (currentTimeStamp - oldTimeStamp) / 1000
+  }
+  oldTimeStamp = currentTimeStamp
 
   fps = Math.round(1 / delta)
 
-  draw(timeStamp)
+  draw(currentTimeStamp)
   updateEntities(delta)
   move()
 
