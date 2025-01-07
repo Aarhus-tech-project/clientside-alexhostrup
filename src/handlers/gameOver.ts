287639f42a6ef41c1354f2213bfa7ec6736b24c3
Alexander Hostrup <alexander.hostrup@gmail.com>
2025-01-07 22:55:34 +0100

    Finished the game over handler

--- src/handlers/gameOver.ts
+++ src/handlers/gameOver.ts
@@ -1,3 +1,15 @@
+import { drawGameOverScreen } from "../graphics/ui"
+import { init } from "../main"
+
 export let isRunning: boolean
 
 export const setIsRunning = (state: boolean) => isRunning = state
+
+export const gameOver = () => {
+    drawGameOverScreen()
+}
+
+export const restartGame = () => {
+    isRunning = true
+    init()
+}
