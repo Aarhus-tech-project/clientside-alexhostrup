c04e899d158a1b71189e62378a21aec6923a8763
Alexander Hostrup <alh@priceshape.dk>
2025-01-07 15:39:20 +0100

    Added a bunch of debugging functions

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,5 +1,5 @@
-import { drawCircle, drawText } from "../handlers/drawing"
-import { context, playerPos } from "../main"
+import { drawCircle, drawRect, drawText } from "../handlers/drawing"
+import { canvas, delta, fps } from "../main"
 import { playerInfo } from "./player"
 
 const yStart = 20
@@ -7,20 +7,31 @@ const gap = 20
 
 let debugInfo: string[] = []
 
+export const prepareDebugInfo = (timeStamp: number) => {
+    addToDebugInfo(`FPS: ${fps}`)
+    addToDebugInfo(`Timestamp: ${timeStamp}`)
+    addToDebugInfo(`Player Height ${playerInfo.height}`)
+    addToDebugInfo(`Player Width ${playerInfo.width}`)
+    addToDebugInfo(`Player X ${playerInfo.position.x}`)
+    addToDebugInfo(`Player Y ${playerInfo.position.y}`)
+    addToDebugInfo(`Canvas size: [height: ${canvas.height}, width: ${canvas.width}]`)
+    addToDebugInfo(`Delta: ${delta}`)
+
+    drawDebugInfo()
+}
+
 export const drawDebugInfo = () => {
     for (let i = 0; i < debugInfo.length; i++) {
         drawText(debugInfo[i], { y: yStart + gap * i })
     }
     debugInfo = []
-    drawPlayerBorder()
-    drawCircle(playerPos, 5)
+    // Draw the player border
+    drawRect(playerInfo.position, { w: playerInfo.width, h: playerInfo.height })
+    // Draw the hitbox border
+    drawRect(playerInfo.hBox?.position!, { w: playerInfo.hBox?.width!, h: playerInfo.hBox?.height! }, 'blue')
+    drawCircle(playerInfo.position, 5)
 }
 
 export const addToDebugInfo = (debugLine: string) => {
     debugInfo.push(debugLine)
-}
-
-const drawPlayerBorder = () => {
-    context.strokeStyle = 'red'
-    context.strokeRect(playerPos.x, playerPos.y, playerInfo.width, playerInfo.height)
 }
