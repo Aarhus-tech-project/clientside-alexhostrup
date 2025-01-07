69f990f665e03666b04eb04330d277ad17c16607
Alexander Hostrup <alexander.hostrup@gmail.com>
2025-01-07 21:12:59 +0100

    Made sure that delta is a number before getting passed into the update functions

--- src/utils/updater.ts
+++ src/utils/updater.ts
@@ -1,9 +1,13 @@
 import { updateMountains } from "../graphics/mountain"
+import { updateScore } from "../graphics/ui"
 import { updateObstacles } from "../handlers/obstacleHandler"
 import { updatePlayer } from "./player"
 
 export const updateEntities = (delta: number) => {
-    updatePlayer(delta)
-    updateMountains(delta)
-    updateObstacles(delta)
+    if (delta) {
+        updatePlayer(delta)
+        updateMountains(delta)
+        updateObstacles(delta)
+        updateScore(delta)
+    }
 }
